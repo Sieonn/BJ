c3f364ba3a062a6cc09ac35f6327e76666ce8839
Sieon <sieon.eve@gmail.com>
2024-06-18 20:34:21 +0900

    Update QueueTest2.java

--- workspace/algo/day0618/src/QueueTest2.java
+++ workspace/algo/day0618/src/QueueTest2.java
@@ -46,3 +46,49 @@ public class QueueTest2 {
 //213
 //1324
 //32415
+
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//public class CandyQueue {
+//    public static void main(String[] args) {
+//        int totalCandies = 20;
+//        Queue<Person> queue = new LinkedList<>();
+//        
+//        // 초기 상태: 1번만 줄을 섬
+//        queue.add(new Person(1, 0));
+//        int currentCandy = 0;
+//        
+//        while (currentCandy < totalCandies) {
+//            // 줄의 첫 번째 사람을 가져옴
+//            Person person = queue.poll();
+//            
+//            // 캔디를 받음
+//            person.candyCount++;
+//            currentCandy += person.candyCount;
+//            
+//            // 다시 줄을 섬
+//            queue.add(person);
+//            
+//            // 새로운 사람이 줄을 섬
+//            if (queue.size() <= totalCandies - currentCandy) {
+//                queue.add(new Person(queue.size() + 1, 0));
+//            }
+//            
+//            // 마지막 캔디를 받는 사람을 출력
+//            if (currentCandy >= totalCandies) {
+//                System.out.println("마지막 캔디를 받은 사람: " + person.id + "번");
+//            }
+//        }
+//    }
+//}
+//
+//class Person {
+//    int id;
+//    int candyCount;
+//    
+//    public Person(int id, int candyCount) {
+//        this.id = id;
+//        this.candyCount = candyCount;
+//    }
+//}
