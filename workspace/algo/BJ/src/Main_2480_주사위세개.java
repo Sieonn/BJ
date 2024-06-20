dcd9b1b6a36376efce319c4d8109a3eb04be3b06
Sieon <sieon.eve@gmail.com>
2024-06-20 16:20:43 +0900

    update

--- workspace/algo/BJ/src/Main_2480_주사위세개.java
+++ workspace/algo/BJ/src/Main_2480_주사위세개.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 
 public class Main_2480_주사위세개 {
 
 	public static void main(String[] args) {
-		int a = (int) (Math.random() * 6) + 1;
-		int b = (int) (Math.random() * 6) + 1;
-		int c = (int) (Math.random() * 6) + 1;
-		int sum = 0;
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
 
-		
-		
-		System.out.println(sum);
+		if (a == b && a == c && b == c) {
+			System.out.println(10000 + a * 1000);
+		} else if (a == b && a != c || a == c && a != b) {
+			System.out.println(1000 + a * 100);
+		} else if (c == b && c != a) {
+			System.out.println(1000 + c * 100);
+		} else {
+			int d = Math.max(Math.max(a, b), c);
+			System.out.println(d * 100);
+		}
 	}
-
 }
