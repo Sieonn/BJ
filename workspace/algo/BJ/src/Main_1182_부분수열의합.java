be83f307f6634219504b223cbb1496637950b1e5
Sieon <sieon.eve@gmail.com>
2024-06-25 16:58:24 +0900

    update

---
+++ workspace/algo/BJ/src/Main_1182_부분수열의합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main_1182_부분수열의합 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int N = sc.nextInt();
+		int S = sc.nextInt();
+		
+		String str = sc.nextLine();
+		String [] sarr = str.split(" ");
+	}
+
+}
