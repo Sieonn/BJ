066ed583f41240417b361b1a89cd354c6659a35c
Sieon <145124127+Sieonn@users.noreply.github.com>
2024-06-26 19:04:50 +0900

    [Bronze II] Title: 상수, Time: 108 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 두 수가 주어지는데 이들의 순서를 거꾸로해서 큰수 출력
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String [] arrA = A.split(""); 
+		String B = sc.next();
+		String [] arrB = B.split(""); 
+
+		A="";
+		B="";
+		for(int i=2; i>=0; i--){
+			A +=arrA[i];
+			B +=arrB[i];
+		}
+		int M = Math.max(Integer.parseInt(A), Integer.parseInt(B));
+		System.out.println(M);
+		
+//		System.out.println(Arrays.toString(arrA));
+//		System.out.println(Arrays.toString(arrB));
+	}
+
+}
