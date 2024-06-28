be54176684b8f2c6b9863700dc11f87216f017fd
Sieon <sieon.eve@gmail.com>
2024-06-28 17:54:31 +0900

    update

--- workspace/algo/BJ/src/Main_2444.java
+++ workspace/algo/BJ/src/Main_2444.java
@@ -1,29 +1,24 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_2444 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int s = (2 * N - 1) / 2;
-		String[] arr = new String[N * 2 - 1];
-		String str = "";
-		arr[s] = "*";
-		for (int i = 0; i < N; i++) {
-			str = "";
-			arr[s + i] = "*";
-			arr[s - i] = "*";
-			for (int j = 0; j < arr.length; j++) {
-				if (arr[j]==null) {
-					arr[j] = " ";
-					str+=" ";
-				}else {
-					str+=arr[j];
-				}
-			}
-			System.out.println(str);
-//			for(int k=/)
+		Scanner scan = new Scanner(System.in);
+		final int N = scan.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N - i; j++)
+				System.out.print(" ");
+			for (int j = 0; j < i * 2 - 1; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+		for(int i=N-1; i >= 0; i--) {
+			for(int j=0; j<N-i; j++)
+				System.out.print(" ");
+			for(int j=0; j<i*2-1; j++)
+				System.out.print("*");
+			System.out.println();
 		}
 	}
 
---
+++ workspace/algo/day0628/src/Main_1753_최단경로.java
@@ -0,0 +1,8 @@
+
+public class Main_1753_최단경로 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
