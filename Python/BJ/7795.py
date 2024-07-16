9ec59fea0010492fc14880da1313d20fa883ef67
sieon <sieon.eve@gmail.com>
2024-07-16 17:39:17 +0900

    update

--- Python/BJ/7795.py
+++ Python/BJ/7795.py
@@ -1,15 +1,22 @@
+# import sys
+# input = sys.stdin.readline
+# tc  = int(input().strip())
+# for _ in range(tc):
+#     A,B = map(int, input().strip().split())
+#     Aarr = sorted(list(map(int, input().strip().split())),reverse=True)
+#     Barr = sorted(list(map(int, input().strip().split())))
+#     cunt = 0
+#     for i in Aarr:
+#         for j in Barr:
+#             if i> j:
+#                 cunt+=1
+#             elif i<=j:
+#                 break
+#     print(cunt)
+
+# 시간초과 발생
+# 이진탐색을 사용해서 풀이
 import sys
-input = sys.stdin.readline
-tc  = int(input().strip())
-for _ in range(tc):
-    A,B = map(int, input().strip().split())
-    Aarr = sorted(list(map(int, input().strip().split())),reverse=True)
-    Barr = sorted(list(map(int, input().strip().split())))
-    cunt = 0
-    for i in Aarr:
-        for j in Barr:
-            if i> j:
-                cunt+=1
-            elif i<=j:
-                break
-    print(cunt)
+import bisect
+
+
