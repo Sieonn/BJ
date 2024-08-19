b1852d5bffa10d6f29f0afb5b7dfff3ed5cade54
임시언 <145124127+Sieonn@users.noreply.github.com>
2024-08-19 21:51:06 +0900

    [Silver II] Title: 정수 삼각형, Time: 96 ms, Memory: 43808 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,20 @@
+# 정수 삼각형
+#현재 수에서 왼쪽 혹은 오른쪽만 선택가능 => 가운데 선택
+
+import sys
+import math
+input = sys.stdin.readline;
+def triangle(D):
+    dp = [[0] * D for i in range(D)]
+    #일단 D단계 내려가니까 생성
+    dp[0][0] = paths[0][0]
+    for i in range(1,D):
+        for j in range(len(paths[i])):
+            #i=2 j=1
+            dp[i][j] = max(dp[i-1][j-1]+paths[i][j], dp[i-1][j]+paths[i][j])
+    return max(dp[D-1])
+D = int(input().strip());
+paths = []
+for i in range(D):
+    paths.append(list(map(int, input().strip().split())));
+print(triangle(D))
