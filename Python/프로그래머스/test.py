a28e3a50c0f4d69ac1ac50886342df10937852de
sieon <sieon.eve@gmail.com>
2024-08-29 17:05:44 +0900

    d

--- Python/프로그래머스/test.py
+++ Python/프로그래머스/test.py
@@ -1,4 +1,31 @@
-def solution(array):
-    if len()
+# def solution(array):
+#     if len()
     
-print(solution([1, 2, 3, 3, 3, 4]))
+# print(solution([1, 2, 3, 3, 3, 4]))
+
+# import math
+# n=144
+
+# b= int(math.sqrt(n))
+# n%b==0
+# if type(b) == int:
+    
+#     print(b)
+# else:
+#     print("no")
+
+# listd = ["1", "2", "g"]
+# liste = ["1", "3", "4", "g"]
+# list2 = set(liste+listd)
+# print(list2)
+
+# def solution(numbers, direction):
+#     return numbers[-1] + numbers[:-1] if direction == "right" else numbers[1:] + numbers[0]
+# print(solution([1, 2, 3], "right"))
+
+# def solution(numbers, direction):
+#     return numbers[-1] + numbers[:len(numbers)-1] if direction == "right" else numbers[1:] + numbers[0]
+# print(solution([1, 2, 3], "right"))
+
+listd = [1, 2, 3]
+print(listd[1])
