880aa4c61b8164072d4070b39754d419cce2a5e6
sieon <sieon.eve@gmail.com>
2024-09-05 09:16:48 +0900

    update

--- Python/프로그래머스/test.py
+++ Python/프로그래머스/test.py
@@ -37,14 +37,27 @@
 #     return new
 # print(solution(23))
 
-def solution(emergency):
-    arr = {}
+# def solution(emergency):
+#     arr = {}
     
-    for j,k in enumerate(sorted(emergency)):
-        arr[k] = len(emergency)-j
-        print(arr)
-    arr2 =[]
-    for i in emergency:
-        arr2 += [arr[i]]
-    return arr2
-print(solution([3, 72,24]))
+#     for j,k in enumerate(sorted(emergency)):
+#         arr[k] = len(emergency)-j
+#         print(arr)
+#     arr2 =[]
+#     for i in emergency:
+#         arr2 += [arr[i]]
+#     return arr2
+# print(solution([3, 72,24]))
+
+# S = "0001100"
+# S=S.split('1')
+# print(S)
+# "000 1 0 1 00 1 0 11 0" => 5,4, 
+# "111 0 1 0 11 0 1 00 1" => 4+1, 5+1  
+import math
+fac = math.factorial
+def solution(balls, share):
+    return fac(balls)/(fac(balls-share)*fac(share))
+ball=3
+share=2
+print(solution(ball,share))
