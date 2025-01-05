5ead9c68a099894c39a67faae95a9061c7571e34
임시언 <145124127+Sieonn@users.noreply.github.com>
2025-01-05 21:46:22 +0900

    [level 0] Title: 이진수 더하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -0,0 +1,20 @@
+def binary(bin):
+    num = 0
+    for i, v in enumerate(bin):
+        if v == "1":
+            num += 2 ** (len(bin) - i -1)
+    return num
+def solution(bin1, bin2):
+    if bin1 == "0" and bin2 == "0":
+        return "0"
+    answer = ""
+    bin3 = binary(bin1) + binary(bin2)
+    while bin3 >= 2 :
+        if bin3 % 2 == 0:
+            bin3 = bin3 // 2
+            answer += "0"
+        else:
+            bin3= bin3 // 2
+            answer += "1"
+    answer += "1"
+    return answer[::-1]
