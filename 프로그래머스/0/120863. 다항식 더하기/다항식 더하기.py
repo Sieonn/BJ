ce15bb7c920d0b24f4fb45a75f0cac5d357bc07e
임시언 <145124127+Sieonn@users.noreply.github.com>
2025-01-08 01:27:41 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,25 @@
+def solution(polynomial):
+    ploy = list(polynomial.split(" + "))
+    #+ 양옆에 공백에 있다는 것을 유의의
+    print(ploy)
+    a = 0
+    b = 0
+    for i in ploy:
+        if "x" in i:
+            i = i.replace("x", "")
+            if i == "":
+                i = 1
+            a += int(i)
+        else:
+            b += int(i)
+            
+    if a == 1:
+        a = "x"
+    elif a > 1:
+        a = str(a)+ "x"
+    else:
+        return str(b)    
+    if b == 0:
+        return a
+    else:
+        return a + " + " + str(b)
